d89a02a9983a4ecd8452e7a6d70eac2e6f11d3b8
Miguel Ryan Bancolita <mr.bancolita@gmail.com>
2023-06-13 00:13:44 +0800

    feat: initial project set up

---
+++ script.js
@@ -0,0 +1,8 @@
+var searchButton = document.getElementById("search");
+
+searchButton.addEventListener("click", searchClicked);
+
+function searchClicked() {
+	const queryText = document.getElementById("query");
+	// TODO: Query API to return list of shows
+}

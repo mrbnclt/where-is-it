0a6d9a02c35c2b9d013ba4aba891a94762a6b29d
Miguel Ryan Bancolita <mr.bancolita@gmail.com>
2023-06-13 22:12:19 +0800

    feat: added user region label

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 var searchButton = document.getElementById("search");
-
 searchButton.addEventListener("click", searchClicked);
+
+let userRegion;
+
+fetch("https://ipinfo.io/json").then(async (response) => {
+	const currentRegion = await response.json();
+	const regionText = document.getElementById("region");
+	var image = document.createElement("img");
+	image.setAttribute("src", "images/hydrangeas.jpg");
+	image.setAttribute("height", "24");
+	image.setAttribute("width", "24");
+	image.setAttribute("alt", "Flower");
+	image.src = `https://flagsapi.com/${currentRegion.country}/shiny/64.png`;
+	document.getElementById("region").appendChild(image);
+	regionText.innerHTML = regionText.innerHTML + ` ${currentRegion.country}`;
+	userRegion = currentRegion.country;
+});
 
 function searchClicked() {
 	const queryText = document.getElementById("query");

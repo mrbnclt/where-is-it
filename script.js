343aad8f09bc8c5526b87445593c710a612e34f9
Miguel Ryan Bancolita <mr.bancolita@gmail.com>
2023-06-14 00:48:39 +0800

    chore: removed unnecessary attributes

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ fetch("https://ipinfo.io/json").then(async (response) => {
 	const currentRegion = await response.json();
 	const regionText = document.getElementById("region");
 	var image = document.createElement("img");
-	image.setAttribute("src", "images/hydrangeas.jpg");
 	image.setAttribute("height", "24");
 	image.setAttribute("width", "24");
-	image.setAttribute("alt", "Flower");
+	image.setAttribute("alt", "Flag");
 	image.src = `https://flagsapi.com/${currentRegion.country}/shiny/64.png`;
 	document.getElementById("region").appendChild(image);
 	regionText.innerHTML = regionText.innerHTML + ` ${currentRegion.country}`;
